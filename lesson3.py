c4309261079a72e2b56fad9fe38906d90d7212a8
George Ruck <georgeadruck@gmail.com>
2020-03-28 12:34:39 +0100

    Changed background colour and used pygame update, we will next add photos and such to the pygame window

--- lesson3.py
+++ lesson3.py
@@ -13,20 +13,29 @@ import pygame
 pygame.init()
 
 #create the screen that we want to play on
+#notice that after inserting this line we might only see the window briefly
+#nothing is telling the programme to keep running!
 screen = pygame.display.set_mode((800,600))
 
 #1 Title and Icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load("CVphoto.jpg")
+icon = pygame.image.load("CVphoto.jpg") #use python emblem
 pygame.display.set_icon(icon)
 
 #unless we create a quit functionality we can't shut the window
 running = True
 #make sure the screen stays up
 while running:
-    #turn false with the close event
+    #the game keeps running, we press buttons that get stored in event!
     for event in pygame.event.get(): #keep looping through the lise of events
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:#stop running the while loop if we close it
             running = False
-            
+    
+    #lets modify what this background looks like
+    #try color to rgb!        
+    screen.fill((0,200,0)) #rgb like we did before!
+    pygame.display.update() #otherwise the pygame screeen will stay black
+    
+    
+    
     
