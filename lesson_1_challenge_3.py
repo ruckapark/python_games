6fcc71be3c31c1f1e9a7aba9b22ca93ad57d6d72
George Ruck <georgeadruck@gmail.com>
2020-03-31 16:35:20 +0200

    Code for analysing text files included as a challenge 4 (should add simple errors for them to debug

---
+++ lesson_1_challenge_3.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 12:51:29 2020
+
+@author: George
+"""
+
+"""
+For this challenge we will need:
+    1. variable assignment
+    2. lists
+    3. Indices
+    
+
+Make a script that does the following:
+    
+1. Prompts the user to enter three strings
+2. Stores the results in variables
+3. Creates a list containing the lengths of each string
+4. Stores the list in a variable
+5. Outputs the list
+6. For each string, outputs
+    * the length (from the stored list)
+    * the first three letters
+    * the last three letters
+    
+"""
+
+# name three strings and think of the input() function
+
+
+# add the strings to a list between square brackets - give the list a sensible name!
+
+
+# print the list
+
+
+# print lenght, first three and last three letters of any string with more that 4 letters
---
+++ lesson_1_challenge_4.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 12:51:29 2020
+
+@author: George
+"""
+
+"""
+I have written this code that tells us how many times dog appears in a certain text
+
+There seem to be a few problems with it though, let's try to debug it!
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+os.chdir('articles')
+
+text_files = [file for file in os.listdir() if 'txt' in file]
+
+article_dict = {}
+article_paramters = {'newspaper':[],'word_length':[],'sentences':[],'ands':[]}
+for file in text_files:
+    temp = open(file,'r')
+    article_dict.update({file:temp.read()})
+    
+min_length = min([len(article_dict[file]) for file in text_files])
+    
+for file in text_files:
+    article_dict[file] = article_dict[file][:min_length]
+    article_paramters['newspaper'].append(file.split('.')[0])
+    article_paramters['word_length'].append(np.mean([len(word) for word in article_dict[file].split()]))
+    article_paramters['sentences'].append(len(article_dict[file].split('.')))
+    article_paramters['ands'].append(len([word for word in article_dict[file].split() if 'and' in word.lower()]))
+ 
+#plotting
+x_locs = np.array([0,1,2])
+barwidth = 0.25
+fig, ax = plt.subplots(1,3,sharey = True)
+
+ax[0].bar(x_locs,article_paramters['word_length'],color = ['r','b','y'])
+ax[0].set_title('Mean word length')
+ax[1].bar(x_locs,article_paramters['sentences'],color = ['r','b','y'])
+ax[1].set_title('Sentences')
+ax[2].bar(x_locs,article_paramters['ands'],color = ['r','b','y'])
+ax[2].set_title('Ands')
+
+plt.show()
+os.chdir('..')
